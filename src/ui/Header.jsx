7f236bd9bf71e08d0b321042bb60a41c0a35a3c6
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-18 18:37:30 +0000

    increase x margins in header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,7 +3,7 @@ import Logo from "./Logo";
 function Header() {
   return (
     <header className="col-span-2 shadow-lg">
-      <div className="flex items-center justify-between mx-5">
+      <div className="flex items-center justify-between mx-8">
         <Logo />
         <h1>App Layout</h1>
       </div>
--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -8,7 +8,7 @@ Logo.propTypes = {
 function Logo({ type = "medium" }) {
   const size = {
     small: "w-20 h-10",
-    medium: "w-44 h-20",
+    medium: "w-44 h-24",
     large: "w-64 h-32",
   };
   return <img src={LogoImg} alt="logo" className={`${size[type]}`} />;
