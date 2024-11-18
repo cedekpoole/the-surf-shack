373bcbd3a2c992b26ced7e5b000f05f8e0581445
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-18 14:05:49 +0000

    import google fonts + extend colours in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pacifico: ["Pacifico", "cursive"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          light: "#388FC4",
+          DEFAULT: "#2B6E97",
+          dark: "#245B7D",
+        },
+        secondary: {
+          light: "#F7D9C4",
+          DEFAULT: "#F4BBAF",
+          dark: "#EFAE9D",
+        },
+        accent: {
+          light: "#F9F871",
+          DEFAULT: "#F7F53D",
+          dark: "#F5F22C",
+        },
+      },
+    },
   },
   plugins: [],
 };
