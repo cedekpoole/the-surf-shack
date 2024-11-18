f7546a80137ce9fab29b6f1ee0ea9b1c46f4e1d1
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-18 21:49:34 +0000

    increase margins on x axis

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,7 +3,7 @@ import Logo from "./Logo";
 function Header() {
   return (
     <header className="col-span-2 shadow-lg">
-      <div className="flex items-center justify-between mx-8">
+      <div className="flex items-center justify-between mx-10">
         <Logo />
         <h1>App Layout</h1>
       </div>
