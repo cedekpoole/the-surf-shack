09b741964f561e4fda7a7fde6e4b9901585595fd
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-19 14:38:39 +0000

    style cabin table

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -15,7 +15,7 @@ function CabinRow({ cabin }) {
   return (
     <div className="grid grid-cols-[0.6fr_1.8fr_2.2fr_1fr_1fr_1fr] px-4 py-3 items-center">
       <img
-        className="aspect-square w-14 block rounded-r-md object-cover object-center transform scale-150 translate-x-[-7px]"
+        className="aspect-square w-12 block rounded-r-md object-cover object-center transform scale-150 translate-x-[-7px]"
         src={image}
         alt="cabin image"
       />
