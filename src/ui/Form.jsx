dcbde21345ee3e94fdad9ffe6b3c5050e9019631
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-21 18:01:25 +0000

    add onSubmit prop to form component

--- src/ui/Form.jsx
+++ src/ui/Form.jsx
@@ -2,11 +2,15 @@ import PropTypes from "prop-types";
 
 Form.propTypes = {
   children: PropTypes.node,
+  onSubmit: PropTypes.func,
 };
 
-function Form({ children }) {
+function Form({ children, onSubmit }) {
   return (
-    <form className="overflow-hidden text-md w-[60rem] border-[1px] border-[#374151] shadow-md px-3 rounded-md">
+    <form
+      onSubmit={onSubmit}
+      className="overflow-hidden text-md w-[60rem] border-[1px] border-[#374151] shadow-md px-3 rounded-md"
+    >
       {children}
     </form>
   );
