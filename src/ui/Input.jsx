337c9ae9884bf71f01b56fbc2830c053f7cf0e3f
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-21 17:58:15 +0000

    use forward ref to fix ref bug

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -1,4 +1,52 @@
 import PropTypes from "prop-types";
+import { forwardRef } from "react";
+
+const Input = forwardRef(
+  (
+    { type, id, defaultValue, disabled = false, element = "input", ...props },
+    ref
+  ) => {
+    const fileInput = type === "file";
+    const textArea = element === "textarea";
+    const className = `rounded-md border border-[#374151] bg-slate-300 shadow-sm focus:outline-none focus:ring-2 text-[#242424] focus:ring-[#374151] transition-all duration-300`;
+
+    if (textArea) {
+      return (
+        <textarea
+          ref={ref}
+          className={`${className} px-3 py-2 h-32 resize-none`}
+          id={id}
+          disabled={disabled}
+          {...props}
+        />
+      );
+    }
+    if (fileInput) {
+      return (
+        <input
+          ref={ref}
+          type={type}
+          id={id}
+          disabled={disabled}
+          className="text-base file:font-medium file:px-3 file:py-2 file:mr-3 file:rounded-md file:border-0 file:text-white file:bg-primary file:cursor-pointer file:transition-colors file:hover:bg-primary-light"
+          {...props}
+        />
+      );
+    }
+
+    return (
+      <input
+        ref={ref}
+        className={`${className} px-4 py-2`}
+        id={id}
+        type={type}
+        defaultValue={defaultValue}
+        disabled={disabled}
+        {...props}
+      />
+    );
+  }
+);
 
 Input.propTypes = {
   type: PropTypes.oneOf(["text", "number", "file"]),
@@ -8,50 +56,6 @@ Input.propTypes = {
   element: PropTypes.oneOf(["input", "textarea"]),
 };
 
-function Input({
-  type,
-  id,
-  defaultValue,
-  disabled = false,
-  element = "input",
-  ...props
-}) {
-  const fileInput = type === "file";
-  const textArea = element === "textarea";
-  const className = `rounded-md border border-[#374151] bg-slate-300 shadow-sm focus:outline-none focus:ring-2 text-[#242424] focus:ring-[#374151] transition-all duration-300`;
-
-  if (textArea) {
-    return (
-      <textarea
-        className={`${className} px-3 py-2 h-32 resize-none`}
-        id={id}
-        disabled={disabled}
-        {...props}
-      />
-    );
-  }
-  if (fileInput) {
-    return (
-      <input
-        type={type}
-        id={id}
-        disabled={disabled}
-        className="text-base file:font-medium file:px-3 file:py-2 file:mr-3 file:rounded-md file:border-0 file:text-white file:bg-primary file:cursor-pointer file:transition-colors file:hover:bg-primary-light"
-        {...props}
-      />
-    );
-  }
-
-  return (
-    <input
-      className={`${className} px-4 py-2`}
-      id={id}
-      type={type}
-      defaultValue={defaultValue}
-      disabled={disabled}
-      {...props}
-    />
-  );
-}
+Input.displayName = "Input";
 
 export default Input;
