fcebef721817fdb2667dec28a14eb7c9d49c2adb
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-21 21:20:27 +0000

    allow image upload in form

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,5 @@
 import supabase from "./supabase";
+import { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
@@ -15,12 +16,14 @@ export async function createCabin(cabin) {
     ""
   );
 
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
   // https://ebzzdomqsbizhnkdjxva.supabase.co/storage/v1/object/public/cabin-images/cabin-003.jpg
 
   //1. create the cabin
   const { data, error } = await supabase
     .from("cabins")
-    .insert([cabin])
+    .insert([{ ...cabin, image: imagePath }])
     .select();
 
   if (error) {
@@ -29,6 +32,15 @@ export async function createCabin(cabin) {
   }
 
   // 2. upload the image
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, cabin.image);
+
+  //3. delete cabin if there was an error uploading the image
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data[0].id);
+    throw new Error("Image could not be uploaded - the cabin was not created");
+  }
 
   return data;
 }
