e4bc19481b2abed2a0c16964a44cc0002c6e7f24
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-25 20:23:25 +0000

    use modal compound componet for cabin form

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,11 +2,11 @@ import PropTypes from "prop-types";
 import { formatCurrency } from "../../utils/helpers";
 import { ImBin } from "react-icons/im";
 import { FaEdit } from "react-icons/fa";
-import { useState } from "react";
 import CreateCabinForm from "./CreateCabinForm";
 import { useDeleteCabin } from "./useDeleteCabin";
 import { IoDuplicate } from "react-icons/io5";
 import { useCreateCabin } from "./useCreateCabin";
+import Modal from "../../ui/Modal";
 
 CabinRow.propTypes = {
   cabin: PropTypes.shape({
@@ -21,7 +21,6 @@ CabinRow.propTypes = {
 };
 
 function CabinRow({ cabin }) {
-  const [showForm, setShowForm] = useState(false);
   const { isDeleting, deleteCabin } = useDeleteCabin();
   const { isCreating, createCabin } = useCreateCabin();
   const {
@@ -46,40 +45,41 @@ function CabinRow({ cabin }) {
   }
 
   return (
-    <>
-      <div className="grid grid-cols-[0.8fr_1.8fr_2.2fr_1fr_1fr_1fr] px-4 py-3 items-center border-b-[1px] border-[#374151]">
-        <img
-          className="border-r-[1px] border-[#374151] aspect-[3/2] w-16 block object-cover object-center transform scale-150 translate-x-[-7px]"
-          src={image}
-          alt="cabin image"
-        />
-        <div className="font-semibold text-lg tracking-wide">{name}</div>
-        <div>Fits up to {maxCapacity} guests</div>
-        <div>{formatCurrency(regularPrice)}</div>
-        {discount ? (
-          <div>{formatCurrency(discount)}</div>
-        ) : (
-          <span className="ml-2">&mdash;</span>
-        )}
-        <div className="flex justify-between">
-          <button
-            disabled={isCreating}
-            onClick={handleDuplicate}
-            className="transition hover:scale-110"
-          >
-            <span className="text-accent-dark hover:text-accent-light text-lg">
-              <IoDuplicate />
-            </span>
-          </button>
-          <button
-            disabled={isDeleting}
-            onClick={() => setShowForm(!showForm)}
-            className="transition hover:scale-110"
-          >
-            <span className="text-accent-dark hover:text-accent-light text-lg">
-              <FaEdit />
-            </span>
-          </button>
+    <div className="grid grid-cols-[0.8fr_1.8fr_2.2fr_1fr_1fr_1fr] px-4 py-3 items-center border-b-[1px] border-[#374151]">
+      <img
+        className="border-r-[1px] border-[#374151] aspect-[3/2] w-16 block object-cover object-center transform scale-150 translate-x-[-7px]"
+        src={image}
+        alt="cabin image"
+      />
+      <div className="font-semibold text-lg tracking-wide">{name}</div>
+      <div>Fits up to {maxCapacity} guests</div>
+      <div>{formatCurrency(regularPrice)}</div>
+      {discount ? (
+        <div>{formatCurrency(discount)}</div>
+      ) : (
+        <span className="ml-2">&mdash;</span>
+      )}
+      <div className="flex justify-between">
+        <button
+          disabled={isCreating}
+          onClick={handleDuplicate}
+          className="transition hover:scale-110"
+        >
+          <span className="text-accent-dark hover:text-accent-light text-lg">
+            <IoDuplicate />
+          </span>
+        </button>
+        <Modal>
+          <Modal.Open opens="edit">
+            <button className="transition hover:scale-110">
+              <span className="text-accent-dark hover:text-accent-light text-lg">
+                <FaEdit />
+              </span>
+            </button>
+          </Modal.Open>
+          <Modal.Window name="edit">
+            <CreateCabinForm cabin={cabin} />
+          </Modal.Window>
           <button
             disabled={isDeleting}
             onClick={() => deleteCabin(cabinId)}
@@ -89,10 +89,9 @@ function CabinRow({ cabin }) {
               <ImBin />
             </span>
           </button>
-        </div>
+        </Modal>
       </div>
-      {showForm && <CreateCabinForm cabin={cabin} />}
-    </>
+    </div>
   );
 }
 
