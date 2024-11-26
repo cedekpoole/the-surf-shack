6e1a0ea3b4e2033be6ca05cb9c60a74b998e9943
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-26 11:19:05 +0000

    reduce page container margin

--- src/ui/PageContainer.jsx
+++ src/ui/PageContainer.jsx
@@ -9,7 +9,7 @@ PageHeader.propTypes = {
 function PageHeader({ children, header, operation }) {
   return (
     <div className="p-4 flex flex-col gap-4">
-      <div className="flex justify-between items-center mb-8">
+      <div className="flex justify-between items-center mb-5">
         <h1 className="text-3xl font-semibold tracking-wide">{header}</h1>
         {operation ? operation : <div></div>}
       </div>
