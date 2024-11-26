81e4b9452645f3109ee8c4774bd1fb10024b682f
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-26 11:40:58 +0000

    add client-side filtering to cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,3 +1,4 @@
+import { useSearchParams } from "react-router-dom";
 import Menus from "../../ui/Menus";
 import Spinner from "../../ui/Spinner";
 import Table from "../../ui/Table";
@@ -6,10 +7,21 @@ import { useCabins } from "./useCabins";
 
 function CabinTable() {
   const { loading, cabins, error } = useCabins();
+  const [searchParams] = useSearchParams();
 
   if (error) return <div>Error: {error.message}</div>;
 
   if (loading) return <Spinner />;
+
+  const filterValue = searchParams.get("discount") || "all";
+
+  let filteredCabins;
+  if (filterValue === "all") filteredCabins = cabins;
+  if (filterValue === "no-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+  if (filterValue === "with-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
   return (
     <Menus>
       <Table>
@@ -22,7 +34,7 @@ function CabinTable() {
           <div></div>
         </Table.Header>
         <Table.Body
-          data={cabins}
+          data={filteredCabins}
           render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />}
         />
       </Table>
