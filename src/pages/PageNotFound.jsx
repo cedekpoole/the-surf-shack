95af7ef21e1a4f6de5cdeb5af046b1daf31d0f48
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-30 13:35:24 +0000

    improve PageNotFound page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,21 @@
+import Button from "../ui/Button";
+import { useMoveBack } from "../hooks/useMoveBack";
+
 function PageNotFound() {
-  return <div>Page not found</div>;
+  const moveBack = useMoveBack();
+  return (
+    <div className="h-screen">
+      <div className="flex items-center justify-center h-full">
+        <div className="text-center">
+          <h1 className="text-9xl font-bold">404</h1>
+          <p className="text-3xl font-semibold">Page Not Found</p>
+          <div className="flex justify-center mt-4">
+            <Button onClick={moveBack}>&larr; Go back</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PageNotFound;
