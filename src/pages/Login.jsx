496eb3d09efb25babbe67a88531d497b539f6c71
cedekpoole <cameron.edek.poole@gmail.com>
2024-12-03 20:58:36 +0000

    add heading to login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,8 +3,11 @@ import Logo from "../ui/Logo";
 
 function Login() {
   return (
-    <main className="bg-[#34434D] min-h-screen flex flex-col gap-10 items-center justify-center">
+    <main className="bg-[#34434D] min-h-screen flex flex-col gap-8 items-center justify-center">
       <Logo type="large" />
+      <h1 className="text-2xl font-semibold tracking-wide">
+        Log into your account
+      </h1>
       <LoginForm />
     </main>
   );
