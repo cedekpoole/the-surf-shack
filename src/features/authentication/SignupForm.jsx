da3e1ea9fede066028d84037f0eef4c0e61fed6b
cedekpoole <cameron.edek.poole@gmail.com>
2024-12-09 15:10:19 +0000

    use react-hook-form for signup form

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -1,22 +1,46 @@
+import { useForm } from "react-hook-form";
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 
 function SignupForm() {
+  const { register, formState } = useForm();
+  const { errors } = formState;
   return (
     <Form>
-      <FormRow label="Full name" error={""}>
-        <Input type="text" id="fullName" />
+      <FormRow label="Full name" error={errors?.fullName?.message}>
+        <Input
+          type="text"
+          id="fullName"
+          {...register("fullName", { required: "This field is required" })}
+        />
       </FormRow>
-      <FormRow label="Email address" error={""}>
-        <Input type="email" id="email" />
+      <FormRow label="Email address" error={errors?.email?.message}>
+        <Input
+          type="email"
+          id="email"
+          {...register("email", { required: "This field is required" })}
+        />
       </FormRow>
-      <FormRow label="Password (min 8 characters)" error={""}>
-        <Input type="password" id="password" />
+      <FormRow
+        label="Password (min 8 characters)"
+        error={errors?.password?.message}
+      >
+        <Input
+          type="password"
+          id="password"
+          {...register("password", { required: "This field is required" })}
+        />
       </FormRow>
-      <FormRow label="Repeat password" error={""}>
-        <Input type="password" id="passwordConfirm" />
+      <FormRow label="Repeat password" error={errors?.passwordConfirm?.message}>
+        <Input
+          type="password"
+          id="passwordConfirm"
+          {...register("passwordConfirm", {
+            required: "This field is required",
+          })}
+        />
       </FormRow>
       <FormRow>
         <Button type="submit" style="primary">
