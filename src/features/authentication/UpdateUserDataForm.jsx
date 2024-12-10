ad4c1a98fa4141cfe8b22a93bc729ba287130d7f
cedekpoole <cameron.edek.poole@gmail.com>
2024-12-10 17:58:10 +0000

    implemement custom hook in update usr form

--- src/features/authentication/UpdateUserDataForm.jsx
+++ src/features/authentication/UpdateUserDataForm.jsx
@@ -4,6 +4,7 @@ import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 import { useUser } from "./useUser";
+import { useUpdateUser } from "./useUpdateUser";
 
 function UpdateUserDataForm() {
   const {
@@ -13,29 +14,49 @@ function UpdateUserDataForm() {
     },
   } = useUser();
 
+  const { updateUser, isUpdating } = useUpdateUser();
+
   const [fullName, setFullName] = useState(currentFullName);
   const [avatar, setAvatar] = useState(null);
 
   function handleSubmit(e) {
     e.preventDefault();
+    if (!fullName) return;
+    updateUser({ fullName, avatar });
   }
+
+  function handleCancel() {
+    setFullName(currentFullName);
+    setAvatar(null);
+  }
+
   return (
     <Form type="large" onSubmit={handleSubmit}>
       <FormRow label="Email address">
         <Input type="email" id="email" disabled defaultValue={email} />
       </FormRow>
       <FormRow label="Full name">
-        <Input type="text" id="fullName" />
+        <Input
+          type="text"
+          id="fullName"
+          onChange={(e) => setFullName(e.target.value)}
+          disabled={isUpdating}
+        />
       </FormRow>
       <FormRow label="Avatar image">
-        <Input type="file" id="image" />
+        <Input
+          type="file"
+          id="image"
+          onChange={(e) => setAvatar(e.target.files[0])}
+          disabled={isUpdating}
+        />
       </FormRow>
       <FormRow>
         <div></div>
-        <Button type="reset" style="secondary">
+        <Button type="reset" style="secondary" onClick={handleCancel}>
           Cancel
         </Button>
-        <Button type="submit" style="primary">
+        <Button type="submit" style="primary" disabled={isUpdating}>
           Update user data
         </Button>
       </FormRow>
