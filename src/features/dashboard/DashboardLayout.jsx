8a55287ad5d7bf86e2aea9f4e0845103fa4addbd
cedekpoole <cameron.edek.poole@gmail.com>
2024-12-10 18:46:15 +0000

    create dashboard layout

---
+++ src/features/dashboard/DashboardLayout.jsx
@@ -0,0 +1,12 @@
+function DashboardLayout() {
+  return (
+    <div className="grid grid-cols-4 grid-rows-[auto_20rem_auto] gap-10">
+      <div>Statistics</div>
+      <div>Today&apos;s activities</div>
+      <div>Chart stay durations</div>
+      <div>Chart revenue</div>
+    </div>
+  );
+}
+
+export default DashboardLayout;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,12 @@
+import DashboardLayout from "../features/dashboard/DashboardLayout";
+import PageContainer from "../ui/PageContainer";
+
 function Dashboard() {
-  return <div>DASHBOARD</div>;
+  return (
+    <PageContainer header="Dashboard">
+      <DashboardLayout />
+    </PageContainer>
+  );
 }
 
 export default Dashboard;
