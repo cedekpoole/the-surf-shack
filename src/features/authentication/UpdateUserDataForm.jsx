18254f68cf496995a6621e0b5c3e17d1663a62f9
cedekpoole <cameron.edek.poole@gmail.com>
2024-12-10 15:17:03 +0000

    set fullname and avatar in state

--- src/features/authentication/UpdateUserDataForm.jsx
+++ src/features/authentication/UpdateUserDataForm.jsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
+import { useUser } from "./useUser";
 
 function UpdateUserDataForm() {
+  const {
+    user: {
+      email,
+      user_metadata: { fullName: currentFullName },
+    },
+  } = useUser();
+
+  const [fullName, setFullName] = useState(currentFullName);
+  const [avatar, setAvatar] = useState(null);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
-    <Form type="large">
+    <Form type="large" onSubmit={handleSubmit}>
       <FormRow label="Email address">
-        <Input type="email" id="email" />
+        <Input type="email" id="email" disabled defaultValue={email} />
       </FormRow>
       <FormRow label="Full name">
         <Input type="text" id="fullName" />
