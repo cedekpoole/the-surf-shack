fa6447cc645adc3e04b843de99f71eec81190614
cedekpoole <cameron.edek.poole@gmail.com>
2024-12-11 21:48:01 +0000

    create useRecentStays hook

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,13 @@
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
+
 function DashboardLayout() {
+  const { bookings, isLoading1 } = useRecentBookings();
+  const { stays, confirmedStays, isLoading: isLoading2 } = useRecentStays();
+
+  if (isLoading1 || isLoading2) return <Spinner />;
+
   return (
     <div className="grid grid-cols-4 grid-rows-[auto_20rem_auto] gap-10">
       <div>Statistics</div>
